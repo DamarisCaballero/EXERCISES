d7e3e193ae2f3646c46938bd5d2874d2ab8f34b4
DamarisCaballero <damariscallero011@gmail.com>
2023-03-02 17:57:52 -0600

    abstraction

---
+++ abstraction.js
@@ -0,0 +1,18 @@
+class Circle {
+    //define data attributes within the constructor
+    constructor (radius = 0) {
+      this.radius = radius;
+      this.pi = 3.142
+    }
+    //define methods
+    getArea() {
+      return this.pi * Math.pow(this.radius, 2);
+    }
+    getPerimeter() {
+      return 2 * this.pi * this.radius;
+    }
+  };
+  
+  const circle = new Circle(5);
+  console.log("Area: " + circle.getArea().toFixed(2));
+  console.log("Perimeter: " + circle.getPerimeter().toFixed(2));
