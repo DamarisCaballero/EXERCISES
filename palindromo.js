70b54e9d4fe78c86acac7894a034c51f96258362
DamarisCaballero <damariscallero011@gmail.com>
2023-05-28 20:02:21 -0600

    FizzBuzz

--- palindromo.js
+++ palindromo.js
@@ -9,3 +9,14 @@ function esPalindromo(cadena) {
 // Ejemplo de uso
 console.log(esPalindromo('Anita lava la tina')); // Devuelve true
 console.log(esPalindromo('Hola mundo')); // Devuelve false
+
+
+/**   
+ * Ejercicio 1: Palíndromo
+Escribe una función en JavaScript que tome una cadena 
+como entrada y determine si es un palíndromo. 
+Un palíndromo es una palabra o frase que se 
+lee igual de izquierda a derecha y viceversa, 
+ignorando los espacios en blanco, la puntuación y
+ el uso de mayúsculas o minúsculas.
+ */
