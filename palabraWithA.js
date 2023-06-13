7272b7f67dcf15f3ef79d98c4b9c97476a0c5634
DamarisCaballero <damariscallero011@gmail.com>
2023-06-12 20:48:29 -0600

    add
    recibe un array de palabras y devuelva true si todas las palabras terminan con la letra "a" y false si al menos una palabra no termina con la letra "a".

---
+++ palabraWithA.js
@@ -0,0 +1,17 @@
+function acabanEnA(words) {
+    for ( let i = 0; i < words.lenght; i++){
+        if (words[i].acabanEnA('a')){
+            return false;
+        }
+    }
+    return true;
+}
+
+const palabras1 = ["casa", "mesa", "silla"];
+console.log(acabanEnA(palabras1)); 
+
+const palabras2 = ["amor", "vida", "familia", "amigo"];
+console.log(acabanEnA(palabras2)); 
+
+const palabras3 = ["gato", "perro", "ratón", "conejo"];
+console.log(acabanEnA(palabras3)); 
