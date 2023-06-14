ea6b559a87a5443aac99c5ae5f524a8bfc5b1272
DamarisCaballero <damariscallero011@gmail.com>
2023-06-13 20:52:55 -0600

    add

--- palabraWithA.js
+++ palabraWithA.js
@@ -15,3 +15,20 @@ console.log(acabanEnA(palabras2));
 
 const palabras3 = ["gato", "perro", "ratón", "conejo"];
 console.log(acabanEnA(palabras3)); 
+
+////////////////
+function acabanEnA(words) {
+    const result = [];
+
+    for (let i = 0; i < words.length; i++) {
+    if (words[i].endsWith('a')) {
+        result.push(words[i]);
+    }
+}
+
+    return result;
+}
+
+console.log(acabanEnA(['casa', 'coche'])); // ["casa", "coche"]
+console.log(acabanEnA(['play', 'balon', 'asa', 'taza', 'tio'])); // ["balon"]
+console.log(acabanEnA(['play', 'midu', 'asa', 'libro', 'lino', 'dino'])); // []
